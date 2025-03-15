9cfcacbdd32238a50a21d6795561ff240693fcba
Kyle Thomas <kyleathomas95@gmail.com>
2025-03-14 17:57:43 -0700

    Initial commit.

---
+++ src/routes/__root.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}
